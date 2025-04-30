307e8c774bce2c0df6db8c503decf95d771c0e2e
KimsakonaSOK <164282648+POTATOES0707@users.noreply.github.com>
2025-04-30 20:47:11 +1000

    new files created

---
+++ Week8_Demo/Week6/js/histogram.js
@@ -0,0 +1,14 @@
+
+
+d3.csv("data/Ex6_TVdata.csv", d => {
+  return {
+    brand: d.brand,
+    model: d.model,
+    screenSize: +d.screenSize,
+    screenTech: d.screenTech,
+    energyConsumption: +d.energyConsumption,
+    star: +d.star,
+  };
+}).then(data => {
+    console.log(data);
+});
